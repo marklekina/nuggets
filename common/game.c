67d9fbbfa747027c629dfc76d83854bd70219e2c
Ava R. Koros <f003xbf@plank.thayer.dartmouth.edu>
2021-11-10 07:26:22 -0500

    commit

--- common/game.c
+++ common/game.c
@@ -20,9 +20,8 @@
 /**************** global types ****************/
 typedef struct game {
   grid_t* grid; // (turns map into string)
-  int spectatorNUM;
   player_t* spectator;
-  char* map; // (txt file)
+  FILE* map; // (txt file)
   player_t* [maxnumber] players; 
 } game_t;
 
@@ -32,24 +31,23 @@ typedef struct game {
  */
 
 game_t*
-game_new(map){
-  if (name == NULL || type == NULL){
+game_new(FILE* map){
+  if (map == NULL ){
     return NULL;
   }
   game_t* game = mem_malloc(sizeof(game_t));
-
-  if(loadMap){
-    edit load map
-    game->grid = map; 
-  }
-  if(player == NULL){
+//POTENTIALLY FIX THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  game->map = map;
+  if(!load_map(map)){
     return NULL;
   }
-  spect = 0;
   else{
-    
-  }
+    game->grid = map; 
+    game->spectator = NULL;
+    game->players = NULL;
 
+  }
+  return game;
 }
 
 /**************** game_delete ****************/
@@ -58,12 +56,21 @@ game_new(map){
  */
 game_t*
 game_delete(game_t* game){
-  //if(NULL !=) free the things in the player
+   if (NULL != game->spectator) {
+    player_delete(game->spectator);
+  }
+  if (NULL != game->players) {
+    for(int i = 0; i<MaxPlayers; i++){
+      player_delete(game->players[i]);
+    }
+  }
   mem_free(game);
 }
 
-void removeSpectator(){
-  delete_player(spectator);
+void removeSpectator(game_t* game){
+  //send message???
+  delete_player(game->spectator);
+
 }
 
 bool
@@ -71,16 +78,64 @@ spectatorAdd(game_t* game, player_t* player){
   if(strcmp(player, "spectator")==1){
     return false
   }
-  if(game->spectatorNum){}
-  if (spectatorNUM!= 1){
-    spectator = player;
-    spectatorNUM = 1;
+  if (game->spectator == NULL){
+    game->spectator = player;
     return true
   }
   else{
-    removeSpectator;
-    spectator = player;
+    removeSpectator(game);
+    game->spectator = player;
     return true;
   }
 
+}
+
+bool
+addPlayer(game_t* game, char* name, char* type){
+  
+}
+	
+
+///getters and setters
+// add player(game and player) 
+// gold distribution
+//variables for gold gold-player just picked up 
+//total gold left 
+//update game, variables 
+
+updategame(){}
+player_t* get__Player(game_t* game, player_t* player){
+
+  if (game == NULL || player == NULL) {
+    return NULL;             
+  } else {
+    int i = 0;
+    while(i<=MaxPlayers && i != NULL){
+        if(strcmp(player_getName(game->players[i]), player_getName(player)==0){
+          return game ->players[i];
+        }
+        i+=1;
+
+      }
+    }
+  return NULL;
+}
+char* game_getMap(game_t* game){
+  if(game!= NULL && game->map != NULL){
+    return game->map;
+  }
+  return NULL;
+}
+char* game_getGrid(game_t* game){
+  if(game!= NULL && game->grid != NULL){
+    return game->grid;
+  }
+  return NULL;
+}
+
+char* game_getSpectator(game_t* game){
+  if(game!= NULL && game->spectator != NULL){
+    return game->spectator;
+  }
+  return NULL;
 }
--- common/player.c
+++ common/player.c
@@ -33,18 +33,21 @@ typedef struct player {
  * see player.h for more information
  */
 player_t*
-player_new(char* name, char* type){
+player_new(char* name, char* type, game_t* game){
   if (name == NULL || type == NULL){
     return NULL;
   }
-  if(strcmp(type, "player")==0 || strcmp(type, "spectator")==0){
+  if(strcmp(type, "player")==1 || strcmp(type, "spectator")==1){
     return NULL;
   }
 
-  if(strlen(name)>= MaxNameLength){
-    return NULL;
+	int count = 0;
+	for(int i = 0; i<MaxNameLength; i++){
+		count ++;
+		if(isgraph(player->name[i]) == 0 && isblank(player->name[i]) ==0){
+			player->name[i] = '_';
+		}
 
-  }
   player_t* player = mem_malloc(sizeof(player_t));
   if(player == NULL){
     return NULL;
@@ -54,7 +57,11 @@ player_new(char* name, char* type){
     player->visibility = NULL;
     player->type = type;
     // are we still doing a global variable for the size of the map? need to give it a limit
-    strand();
+    grid_t* grid = game_getGrid(game);
+    char* map = grid->map;
+    int length = strlen(map);
+    strand(map);
+    grid_delete(grid);
     player->purse = 0;
     player->xPos = rand();
     player->yPos = rand();
@@ -88,6 +95,12 @@ player_delete(player_t* player){
 char* player_getVisibility(player_t* player){
   if(player!= NULL && player->visibility != NULL){
     return player->visibility;
+  }
+  return NULL;
+
+}char* player_getName(player_t* player){
+  if(player!= NULL && player->name != NULL){
+    return player->name;
   }
   return NULL;
 }
@@ -192,4 +205,11 @@ player_setyPos(player_t* player, int yPos){
     return true;
   }
   return false;
+}
+
+void addToPurse(int numGold){
+  if(player->purse <= GoldTotal){
+    player->purse += numGold;
+  }
+
 }
