356e3185e31a9003f88c61f7bbb53dc1df5fd322
Ava R. Koros <f003xbf@plank.thayer.dartmouth.edu>
2021-11-14 23:57:03 -0500

    Ava's changes

--- common/game.h
+++ common/game.h
@@ -27,14 +27,89 @@
 typedef struct pile pile_t;
 typedef struct game game_t;
 
-game_t* game_new(FILE* fp, int nrows, int ncols);
-void game_delete(game_t* game);
-void remove_spectator(game_t* game);
-bool add_spectator(game_t* game);
-int add_player(game_t* game, char* name);
-int get_num_players(game_t* game);
-player_t* get_player(game_t* game, int idx);
-grid_t* get_grid(game_t* game);
-player_t* get_spectator(game_t* game);
+/**************** functions ****************/
 
+/**************** pile_new ****************/
+/* Creates a new game or returns NULL if invalid parameters
+ * Create a new game with the provided map 
+ * nrows and ncols provided help load the game's grid
+ * The spectator is intialized empty
+ * The location is the amount of players currently in the game... intialized to 0
+ * Initialized game is returned
+ */
+pile_t* pile_new(point_t* location, int amount);
+
+/**************** pile_delete****************/
+/* Frees pile and its location
+ */
+void pile_delete(pile_t* pile);
+
+/**************** game_new ****************/
+/* Creates a new game or returns NULL if invalid parameters
+ * Create a new game with the provided file, rows, and column 
+ * nrows and ncols provided help load the game's grid
+ * The spectator is intialized empty
+ * the nuumber of players is initialized to 0
+ * Initialized game is returned
+ */
+game_t* game_new(FILE* fp, int nrows, int ncols);
+
+/**************** game_delete ****************/
+/* 
+ * deletes spectator and players in the player list
+ * deletes game 
+ */
+void game_delete(game_t* game);
+
+/**************** remove_spectator ****************/
+/* 
+ * deletes the game's spectator and sends message telling them
+ */
+void remove_spectator(game_t* game);
+
+/**************** add_spectator****************/
+/* 
+ * Adds a new spectator to the game
+ * Removes current spectator, if there is one
+ * Returns true if the addition is successful
+ */
+bool add_spectator(game_t* game);
+
+/**************** add_player ****************/
+/* 
+ * adds player with given name and type to the running list of players in the game
+ * incriments the number of players within the game
+ * returns the player's number
+ * otherwise returns invalid index
+ */
+int add_player(game_t* game, char* name);
+
+/**************** get_num_player ****************/
+/* 
+ * returns number of players
+ */
+int get_num_players(game_t* game);
+
+/**************** get_Player ****************/
+/* 
+ * returns the player in the location that matches the given index
+ */
+player_t* get_player(game_t* game, int idx);
+
+/**************** game_getGrid ****************/
+/* 
+ * returns the game's grid
+ */
+grid_t* get_grid(game_t* game);
+
+/**************** game_getSpectator ****************/
+/* 
+ * returns the game's spectator
+ */
+player_t* get_spectator(game_t* game);
+/**************** compute_visibility ****************/
+/* 
+ * computes visibility for a given player
+ */
+char* compute_visibility(point_t* player, grid_t* grid);
 #endif // __GAME_H
--- common/player.h
+++ common/player.h
@@ -24,23 +24,119 @@
 #include "mem.h"
 #include "../support/message.h"
 
+/**************** global types ****************/
 typedef struct point point_t;
 typedef struct player player_t;
 
+/**************** functions ****************/
+
+/**************** point_new ****************/
+/* 
+ * intializes x and y to provided values
+ * returns new point
+ */
 point_t* point_new(int x, int y);
-player_t* player_new(char* name, char* type);
-void player_delete(player_t* player);
-char* get_name(player_t* player);
-char* get_type(player_t* player);
-point_t* get_location(player_t* player);
-char* get_visible_map(player_t* player);
-addr_t* get_address(player_t* player);
-int get_purse(player_t* player);
-bool set_location(player_t* player, int x, int y);
-bool set_visible_map(player_t* player, char* visible_map);
-bool set_address(player_t* player, addr_t* to);
-int add_to_purse(player_t* player, int gold_coins);
-bool set_name(player_t* player, char* name);
+
+/**************** get_x ****************/
+/*
+ * returns point's x value
+ */
 int get_x(point_t* point);
+
+/**************** get_y ****************/
+/*
+ * returns point's y value
+ */
 int get_y(point_t* point);
 #endif // __PLAYER_H
+
+
+/**************** player_new ****************/
+/* 
+ * creates a new player, or NULL if error.
+ * The provided name is validated and is set as player's name
+ * The provided type is validated and set 
+ * The visible map, location, address are set to NULL
+ * Purse is set to 0
+ * This then returns the new player or NULL if error 
+ */
+player_t* player_new(char* name, char* type);
+
+/**************** player_delete ****************/
+/*
+ * deletes player as well as their name, type, location, address, and visible map.
+ */
+void player_delete(player_t* player);
+
+/**************** get_name ****************/
+/*
+ * returns player's name 
+ */
+char* get_name(player_t* player);
+
+/**************** get_type ****************/
+/*
+ * returns player's type
+ */
+char* get_type(player_t* player);
+
+/**************** get_location ****************/
+/*
+ * returns player's location
+ */
+point_t* get_location(player_t* player);
+
+/**************** get_visible_map ****************/
+/*
+ * returns player's visible map
+ */
+char* get_visible_map(player_t* player);
+
+/**************** get_address ****************/
+/*
+ * returns player's address
+ */
+addr_t* get_address(player_t* player);
+
+/**************** get_purse ****************/
+/*
+ * returns the value of the players purse
+ */
+int get_purse(player_t* player);
+
+/**************** set_location ****************/
+/*
+ * changes location to a point made from the given x and y values
+ * returns true if successful
+ */
+bool set_location(player_t* player, int x, int y);
+
+/**************** set_visible_map ****************/
+/*
+ * changes visible to the given string
+ * returns true if successful
+ */
+bool set_visible_map(player_t* player, char* visible_map);
+
+/**************** set_address ****************/
+/*
+ * changes address to given address
+ * returns true if successful
+ */
+bool set_address(player_t* player, addr_t* to);
+
+/**************** set_name ****************/
+/*
+ * changes name to the given name
+ * returns true if successful
+ */
+bool set_name(player_t* player, char* name);
+/**************** add_to_purse ****************/
+/*
+ * changes address to given address
+ * returns purse value
+ */
+int add_to_purse(player_t* player, int gold_coins);
+
+
+
